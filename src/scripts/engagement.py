5a29d436b48532dbbe9c7a83242e11b97a76d1b5
AdaraPutri <adarasharmilaputri@gmail.com>
2024-04-01 06:36:14 -0700

    completed engagement script

--- src/scripts/engagement.py
+++ src/scripts/engagement.py
@@ -8,8 +8,7 @@ from src.util.filepath_helpers import get_user_filepath_input, add_stem_to_filen
 def filter_engagement_script():
     
 
-    actions_file_path = pathlib.Path("/Users/jayati/usageAnalysis-1/data/Jan2023-actions_filtered_submissions_attempts.csv")
-    # get_user_filepath_input("Enter the absolute path of your filtered consent csv file: ")
+    actions_file_path = get_user_filepath_input("Enter the absolute path of attempts csv file: ")
     output_filename = add_stem_to_filename(actions_file_path, "engagement")
     output_file_path = get_output_file_path(output_filename)
     
@@ -19,14 +18,19 @@ def filter_engagement(data_file_path, output_file_path):
     data = pd.read_csv(data_file_path)
     
     total = data.groupby(['actor','question.parent_category_name', 'question.category_name', 'question.type_name', 'question.id']).size().reset_index(name='counts')
+    total['accuracy'] = pd.Series(dtype='float')
+
     correct = data[data['data.status'] == "Correct"].groupby(['actor','question.parent_category_name', 'question.category_name', 'question.type_name', 'question.id']).size().reset_index(name='correctcounts')
+
     for index, row in total.iterrows():
         correctrow = correct[(correct['actor'] == row['actor']) & (correct['question.parent_category_name'] == row['question.parent_category_name']) & (correct['question.category_name'] == row['question.category_name']) & (correct['question.type_name'] == row['question.type_name']) & (correct['question.id'] == row['question.id'])]
-        if correctrow['correctcounts'].values :
-            row['counts'] = int(correctrow['correctcounts'])/int(row['counts'])
+        if len(correctrow['correctcounts'].values) >= 1 :
+            total.at[index, 'accuracy'] = int(correctrow['correctcounts'])/int(row['counts'])
         else:
-            row['counts'] = 0
-    total.to_csv(get_output_file_path("test3.csv"), sep=',')
+            total.at[index, 'accuracy'] = 0
+            
+    print(total.head())
+    total.to_csv(output_file_path, sep=',')
 #user, parent, child, type, question id, accuracy, num of attempts
 #take the data, aggregate everything per concept 
 
